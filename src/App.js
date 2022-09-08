c0e7fa3c38da499496812290c90b0dcd98571f5a
serhatbek <serhat.bekk@gmail.com>
2022-09-08 10:52:38 +0300

    reducer

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ const initialState = {
   num: 0,
 };
 
+const reducerFn = (prevState, action) => {
+  switch (action.type) {
+    case 'INCREASE':
+      return { num: prevState.num + action.payload };
+
+    case 'DECREASE':
+      return { num: prevState.num - 1 };
+
+    default:
+      return { num: prevState.num };
+  }
+};
+
 function App() {
   const [state, dispatch] = useReducer(reducerFn, initialState);
 
   return (
     <main>
-      <button onClick={() => dispatch({ type: 'INCREASE' })}>Ekle</button>
-      <button onClick={() => dispatch({ type: 'DECREASE' })}>Çıkar</button>
+      <div>
+        <button onClick={() => dispatch({ type: 'INCREASE', payload: 4 })}>
+          Increase 4
+        </button>
+        <button onClick={() => dispatch({ type: 'DECREASE' })}>
+          Increase 1
+        </button>
+        <p>{state.num}</p>
+      </div>
     </main>
   );
 }
