dc4da802d0703230b2cd5af78f91bf676973e98b
serhatbek <serhat.bekk@gmail.com>
2022-09-08 10:13:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+
+const initialState = {
+  num: 0,
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducerFn, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <button onClick={() => dispatch({ type: 'INCREASE' })}>Ekle</button>
+      <button onClick={() => dispatch({ type: 'DECREASE' })}>Çıkar</button>
+    </main>
   );
 }
 
